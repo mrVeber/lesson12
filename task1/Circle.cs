c55906c065bf802e079d93890d30ba77845deb3d
Pavel Bobylev <paha_ska_15@mail.ru>
2022-04-18 10:35:07 +0300

    Добавьте файлы проекта.

---
+++ task1/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    static class Circle
+    {
+        static public double GetCircumference (double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        static public double GetAreaOfACircle (double r)
+        {
+            return Math.PI * Math.Pow(r, 2);
+        }
+
+        static public string GetOwnershipChek (double r, double x, double y)
+        {
+            double h = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            if (h > r)
+            {
+                return "Точка не принадлежит окружности!";
+            }
+            return "Точка принадлежит окружности!";
+        }
+    }
+}
---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка работоспособности статического класса для работы с окружностью.");
+            Console.Write("Введите радиус окружности: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату Х для точки: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату Y для точки: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write($"Длинна окружности равна: {Circle.GetCircumference(r)}");
+            Console.Write($"\nПлощадь окружности равна: {Circle.GetAreaOfACircle(r)}");
+            Console.Write($"\nПроверка принадлежит ли точка с координатами ({x},{y}) окружности с радиусом {r} : {Circle.GetOwnershipChek(r,x,y)}");
+            Console.ReadKey();
+        }
+    }
+}
